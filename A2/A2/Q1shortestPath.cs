280c9999b4b82872b943bee9373f2c9e5209289b
SepehrHashemi <se.hashemih@gmail.com>
2019-03-02 22:25:36 +0330

    A2

---
+++ A2/A2/Q1shortestPath.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A2
+{
+    public class Q1ShortestPath : Processor
+    {
+        public Q1ShortestPath(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long,long[][], long, long, long>)Solve);
+        
+        public long Solve(long NodeCount, long[][] edges, long StartNode,  long EndNode)
+        {
+            var nodes = new Node[NodeCount];
+            for (int i = 0; i < nodes.Length; i++)
+                nodes[i] = new Node(i + 1, long.MaxValue);
+
+            nodes[StartNode - 1].Distance = 0;
+            var priorityQueue = new PriorityQueue((int)NodeCount, nodes);
+            var Graph = MakeGraph(NodeCount, nodes, edges);
+
+            if (Graph[StartNode].Count == 0)
+                return -1;
+
+            while (priorityQueue.Size > 0)
+            {
+                var currentNode = priorityQueue.ExtractMin();
+                foreach (var node in Graph[currentNode.Data])
+                    if (currentNode.Distance < long.MaxValue)
+                        if (node.Distance > currentNode.Distance + 1)
+                            priorityQueue.ChangePriority(Array.IndexOf(priorityQueue.MinHeap, node), (int)currentNode.Distance + 1);
+            }
+
+            return nodes[(int)EndNode - 1].Distance != long.MaxValue ? nodes[(int)EndNode - 1].Distance : -1;
+        }
+
+        private Dictionary<long, List<Node>> MakeGraph(long nodeCount, Node[] nodes, long[][] edges)
+        {
+            var nodeConnections = new Dictionary<long, List<Node>>();
+            for (int i = 1; i <= nodeCount; i++)
+            {
+                nodeConnections.Add(i, new List<Node>());
+                foreach (var edge in edges)
+                    if (edge.Contains(i))
+                        nodeConnections[i].Add(nodes[edge.Where(x => x != i).First() - 1]);
+            }
+            return nodeConnections;
+        }
+
+    }
+}
---
+++ A2/A2/Q2BipartiteGraph.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A2
+{
+    public class Q2BipartiteGraph : Processor
+    {
+        public Q2BipartiteGraph(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], long>)Solve);
+
+
+        public long Solve(long NodeCount, long[][] edges)
+        {
+            var nodes = new Nullable<bool>[NodeCount + 1];
+            nodes[1] = false;
+            var queue = new Queue<long>();
+            queue.Enqueue(1);
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                for (int i = 0; i < edges.Length; i++)
+                    if (edges[i].Count(x => x == currentNode) > 0)
+                    {
+                        var connectedNode = edges[i].Where(x =>
+                        x != currentNode).First();
+                        var nodeColor = nodes[connectedNode];
+                        if (nodeColor == null)
+                        {
+                            nodes[connectedNode] = !nodes[currentNode];
+                            queue.Enqueue(connectedNode);
+                        }
+                        else if (nodeColor == nodes[currentNode])
+                            return 0;
+                    }
+            }
+            return 1;
+        }
+    }
+
+}
