2d7f580410a820ff4de6bd2fe7903288eaeee38a
SepehrHashemi <se.hashemih@gmail.com>
2019-03-28 02:49:58 +0430

    Q1, Q2 Solved

---
+++ A4/A4/Q1BuildingRoads.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A4
+{
+    public class Q1BuildingRoads : Processor
+    {
+        public Q1BuildingRoads(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], double>)Solve);
+
+
+        public double Solve(long pointCount, long[][] points)
+        {
+            //Write Your Code Here
+            return 0;
+        }
+    }
+}
