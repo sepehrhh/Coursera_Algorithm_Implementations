5e0feec12653f726e81f187eb83f3d6a1fce5c4f
SepehrHashemi <se.hashemih@gmail.com>
2019-04-19 18:56:49 +0430

    Added solution files

---
+++ A7/A7/Q1FindAllOccur.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A7
+{
+    public class Q1FindAllOccur : Processor
+    {
+        public Q1FindAllOccur(string testDataName) : base(testDataName)
+        {
+			this.VerifyResultWithoutOrder = true;
+        }
+
+        public override string Process(string inStr) =>
+        TestTools.Process(inStr, (Func<String, String, long[]>)Solve, "\n");
+
+        public long[] Solve(string text, string pattern)
+        {
+            // write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
